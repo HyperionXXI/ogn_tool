602427dad8b7cd0898b72831bf92a9891f3d7e3a
Florian Keller <ton.email@exemple.com>
2026-03-19 00:48:42 +0100

    refactor(move): remove obsolete analysis/rf_models package wrapper

--- src/ogn_tool/analysis/rf_models/__init__.py
+++
@@ -1,5 +0,0 @@
-from ogn_tool.intelligence.altitude_distance_distribution import *  # noqa: F401,F403
-from ogn_tool.intelligence.radio_horizon_empirical import *  # noqa: F401,F403
-from ogn_tool.intelligence.terrain_classification import *  # noqa: F401,F403
-from ogn_tool.intelligence.terrain_visibility_inference import *  # noqa: F401,F403
-from ogn_tool.kernel.rf_visibility_model import *  # noqa: F401,F403
